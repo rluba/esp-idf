07d9284bf046872ab36be191b3a3ca348e64e777
Raphael Luba <raphael@leanbyte.com>
2024-12-23 10:52:51 +0100

    Fix crashes in i2c ISR because it called non-inlined functions not in IRAM
    This is a backport of an original fix for v4.4. Most of it has been fixed by now in the main branch. But I’m not sure if these functions are still called, so I’m also adding IRAM_ATTR here, to be safe.

--- components/hal/i2c_hal_iram.c
+++ components/hal/i2c_hal_iram.c
@@ -6,12 +6,14 @@
 
 #include "hal/i2c_hal.h"
 
+#include "esp_attr.h"
+
 //////////////////////////////////////////Deprecated Functions//////////////////////////////////////////////////////////
 /////////////////////////////The following functions are only used by the legacy driver/////////////////////////////////
 /////////////////////////////They might be removed in the next major release (ESP-IDF 6.0)//////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void i2c_hal_master_handle_tx_event(i2c_hal_context_t *hal, i2c_intr_event_t *event)
+void IRAM_ATTR i2c_hal_master_handle_tx_event(i2c_hal_context_t *hal, i2c_intr_event_t *event)
 {
     uint32_t intr_status = 0;
     i2c_ll_get_intr_mask(hal->dev, &intr_status);
@@ -28,7 +30,7 @@ void i2c_hal_master_handle_tx_event(i2c_hal_context_t *hal, i2c_intr_event_t *ev
     }
 }
 
-void i2c_hal_master_handle_rx_event(i2c_hal_context_t *hal, i2c_intr_event_t *event)
+void IRAM_ATTR i2c_hal_master_handle_rx_event(i2c_hal_context_t *hal, i2c_intr_event_t *event)
 {
     uint32_t intr_status = 0;
     i2c_ll_get_intr_mask(hal->dev, &intr_status);
